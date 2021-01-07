00a996f25a78d01f07136798211c67b0d332fa20
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-01-07 12:24:47 -0500

    Time: 192 ms (9.31%), Space: 19.9 MB (11.35%) - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -5,6 +5,11 @@ class Solution(object):
         :rtype: bool
         """
         return len(nums) > len(set(nums))
+        # nums.sort()
+        # for i in range(len(nums)):
+        #     if i <= (len(nums)-1):
+        #         return nums[i] == nums[i+1]
+        #         break
 ​
         
         
