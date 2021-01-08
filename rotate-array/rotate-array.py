2ed1ac46a13a0cbb6d95ef61bd67bc5daa31d31f
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-01-08 12:55:43 -0500

    Time: 32 ms (99.94%), Space: 13.7 MB (96.02%) - LeetHub

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -6,11 +6,11 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
 ​
-        if k > len(nums):
-            t = k%len(nums)
-            nums[:] = nums[-t:]+nums[:-t]
-        else:
-            nums[:] = nums[-k:]+nums[:-k]
+​
+        k = k%len(nums)
+        nums[:] = nums[-k:]+nums[:-k]
+​
         
         return nums
+        
         
