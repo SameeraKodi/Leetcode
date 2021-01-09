75d4f95216fd4719f201ff1d9338f807fe1afd04
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-01-09 16:16:17 -0500

    Time: 1012 ms (8.33%), Space: 0B (100.00%) - LeetHub

---
+++ immediate-food-delivery-i/immediate-food-delivery-i.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+​
+select round(sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)*100/count(*),2) as "immediate_percentage"
+from Delivery
+​
+​
+​
+​
+​
