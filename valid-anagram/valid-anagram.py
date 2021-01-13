7c0e090ba812a974930525ef89016bb9f31578f0
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-01-12 21:56:06 -0500

    Time: 6892 ms (5.38%), Space: 14.5 MB (73.82%) - LeetHub

---
+++ valid-anagram/valid-anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict1 = {}
+        dict2 = {}
+        if len(s) == len(t):
+            for i in range(len(s)):
+                dict1[s[i]]  = s.count(s[i])
+                dict2[t[i]] = t.count(t[i])
+            return dict1 == dict2
+                
+        else:
+            return False
+        
+​
+​
+​
+​
+        
