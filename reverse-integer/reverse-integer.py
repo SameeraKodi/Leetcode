e055a84f9c076b1dce08aec7b74425e890ab8193
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-03-31 23:54:44 -0400

    Time: 36 ms (37.82%), Space: 13.9 MB (99.87%) - LeetHub

--- reverse-integer/reverse-integer.py
+++ reverse-integer/reverse-integer.py
@@ -1,15 +1,12 @@
 class Solution:
-    def reverse(self, x: int) -> int:
-        temp = str(x)
-        temp = temp[::-1]
-        if temp[-1] == '-':
-            temp = temp[:-1]
-            temp = -1*int(temp)
-        else:
-            temp = int(temp)
-        if temp>= 2**31 or temp<= -2**31:
-            return 0
-        else:
-            return temp
-​
-        
+    def reverse(self, x: int) -> int:
+        string = str(x)
+        if string[0] == "-":
+            num = int(string[::-1][:-1])*-1
+            
+            return num if num > -2**31 else 0
+        else:
+            num = int(string[::-1])
+            return num if num < 2**31 else 0
+            
+        
