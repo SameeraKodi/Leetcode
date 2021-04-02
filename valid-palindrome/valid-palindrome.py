697775025065c8db9f9fb523510533f4a9635ff3
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-02 19:32:34 -0400

    Time: 52 ms (41.07%), Space: 20.2 MB (6.31%) - LeetHub

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -1,11 +1,18 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        lst =[i for i in s.lower() if i.isalnum()]
-        
-#         for i in s.lower():
-#             if i.isalnum():
-#                 lst.append(i)
-​
-        return lst == lst[::-1]
-​
-        
+    def isPalindrome(self, s: str) -> bool:
+        lst = [i.lower() for i in s if i.isalnum() == True]
+        new = lst.copy()
+        
+        midpoint = int(len(lst)/2)
+
+        for i in range(midpoint):
+            temp = new[i]
+            new[i] = new[-i-1]
+            new[-i-1] = temp
+        
+            
+        return lst == new
+            
+            
+
+        
