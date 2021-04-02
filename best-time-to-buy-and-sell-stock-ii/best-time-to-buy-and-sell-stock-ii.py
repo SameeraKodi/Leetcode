27f5a879ee887f9311dc4876b29a440f05dd015d
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-02 09:11:12 -0400

    Time: 52 ms (97.38%), Space: 15.1 MB (22.83%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,27 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+       #since multiple transactions are allowed we have to keep track of all possible profits hence the final list
         profit = 0
         final = []
         
         for i in range(1,len(prices)):
             buy_price = prices[i-1]
-            if prices[i] > buy_price:
-                #print(prices[i],buy_price)
+            if prices[i] > buy_price: #this is condition implying selling
                 profit = max(profit,prices[i]- buy_price)
-                #print(profit)
                 final.append(profit)
                 profit = 0
-                #print(final)
 
-                
-            else:
+
+            else: #this is condition implying no sale so change in buy price
                 buy_price = prices[i]
                 
         return sum(final)
-
-
-                
-                
-            
-        
