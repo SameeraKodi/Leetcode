736680439ad1165db4acc428774b515037847e6e
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-03 14:31:06 -0400

    Time: 52 ms (20.92%), Space: 14.4 MB (73.01%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,16 +1,30 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-                #you have a target, check if value and target - value exist in the lst and make sure they don't have the same index
-        lst = []
-        for i,value in enumerate(nums):
-            x = target-value
-            if x in nums and i != nums.index(x):
-                lst.append(i)
-                lst.append(nums.index(x))
-                break
-            else:
-                           continue
+#                 #you have a target, check if value and target - value exist in the lst and make sure they don't have the same index
+#         lst = []
+#         for i,value in enumerate(nums):
+#             x = target-value
+#             if x in nums and i != nums.index(x):
+#                 lst.append(i)
+#                 lst.append(nums.index(x))
+#                 break
+#             else:
+#                            continue
                            
-        return lst
+#         return lst
+
+       # dictionary solution
+        dic = {}
+        for i,val in enumerate(nums):
+            x = target-val
+            if x not in dic:
+                dic[val] = i
+            else : 
+                return [i,dic[x]]
+
+ 
+            
+
+            
                 
         
