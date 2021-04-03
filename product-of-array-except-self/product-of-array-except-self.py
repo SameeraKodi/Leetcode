38709b25cbb7ea33de591e92198deb67eef332c8
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-03 10:14:49 -0400

    Time: 320 ms (6.67%), Space: 22.5 MB (7.52%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.py
+++ product-of-array-except-self/product-of-array-except-self.py
@@ -1,24 +1,51 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+#without division ( running left product running right product)
+        l = len(nums)
+        l_prod = [1]*l
+        r_prod = [1]*l
+        
+        for i in range(1,len(nums)):
+            l_prod[i] = nums[i-1]*l_prod[i-1]
 
-        product = 1
-        for i in nums:
-            if i != 0:
-                product = product*i
-            else:
-                product = product*1
+        for k in range(-2,-len(nums)-1,-1):
+            r_prod[k] = nums[k+1]*r_prod[k+1]
+            
+    
+            
+        return [l_prod[i]*r_prod[i] for i in range(len(nums))]
+            
+            
+            
+            
+
+        
+#With division
+
+#         product = 1
+        
+#         for i in nums:
+#             if i != 0:
+#                 product = product*i
+#             else:
+#                 product = product*1
                 
 
             
-        if 0 in nums:
-            if nums.count(0) > 1:
-                return [0*i for i in nums]
-            else:
-                return [0 if p!=0 else product for p in nums]
+#         if 0 in nums:
+#             if nums.count(0) > 1:
+#                 return [0*i for i in nums]
+#             else:
+#                 return [0 if p!=0 else product for p in nums]
             
-            
-        else:
-            return (int(product/k) for k in nums)
+#         #with division   
+#         else:
+#             return [int(product/k) for k in nums]
+        
+
+        
+        
                 
         
 
