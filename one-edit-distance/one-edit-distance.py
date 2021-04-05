d9ed5796313f8e725da959f216767e02d66d1830
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-04 21:25:52 -0400

    Time: 28 ms (88.02%), Space: 14.4 MB (45.63%) - LeetHub

---
+++ one-edit-distance/one-edit-distance.py
@@ -0,0 +1,47 @@
+class Solution:
+    def isOneEditDistance(self, s: str, t: str) -> bool:
+
+        if abs(len(s) - len(t)) > 1:
+            return False
+        if s == t:
+            return False
+        
+        
+        if len(s) < len(t):
+            for i in range(len(s)):
+                if s[i] != t[i]:
+                    if s[i:] == t[i+1:]:
+                        return True
+                    else:
+                        return False
+        
+        
+        elif len(s) > len(t):
+            for i in range(len(t)):
+                if t[i] != s[i]:
+                    if t[i:] == s[i+1:]:
+                        return True
+                    else:
+                        return False
+        else:
+            for i in range(len(s)):
+                if s[i] != t[i]:
+                    if s[i+1:] == t[i+1:]:
+                        return True
+                    else:
+                        return False
+                    
+        return True 
+                    
+                    
+
+                
+        
+                    
+                
+                    
+
+            
+                
+            
+        
