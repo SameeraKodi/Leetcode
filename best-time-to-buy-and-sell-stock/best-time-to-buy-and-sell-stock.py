84ada285a7de67783a92cc528dbb219fa08b54e0
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-11 17:32:18 -0400

    Time: 1016 ms (61.15%), Space: 25.2 MB (52.37%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,19 +1,37 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
- #keep the initital buy price as the first day
         buy_price = prices[0]
-        #initial profit as 0 ( works well for monotonically decreasing case)
         profit = 0
         
-        #now loop through the list ( only once) to update the buy price and profit as we go along
-        for i in range(1,len(prices)):
+        for i in range(len(prices)):
             if prices[i] > buy_price:
                 profit = max(profit, prices[i]-buy_price)
-                
-            #to save the lowest buy_price
             else:
                 buy_price = prices[i]
                 
-                
         return profit
+        
+        
+        
+        
+        
+        
+        
+        
+#  #keep the initital buy price as the first day
+#         buy_price = prices[0]
+#         #initial profit as 0 ( works well for monotonically decreasing case)
+#         profit = 0
+        
+#         #now loop through the list ( only once) to update the buy price and profit as we go along
+#         for i in range(1,len(prices)):
+#             if prices[i] > buy_price:
+#                 profit = max(profit, prices[i]-buy_price)
+                
+#             #to save the lowest buy_price
+#             else:
+#                 buy_price = prices[i]
+                
+                
+#         return profit
         
