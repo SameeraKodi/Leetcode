d207c4d1a34b55b27a54f88c3fc5d5d685178832
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-11 09:54:52 -0400

    Time: 48 ms (58.97%), Space: 14.3 MB (91.14%) - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,12 +1,57 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
+        
         if len(s) != len(t):
             return False
         
-#         lst1 = [i for i in s].sort()
-#         lst2 = [j for j in t].sort()
+        dict_1 = {}
+        #dict_2 = {}
         
-        return sorted(s) == sorted(t)
+
+        for i in range(len(s)):
+            if s[i] not in dict_1:
+                dict_1[s[i]] = 1
+            else:
+                dict_1[s[i]] +=1
+                
+        for j in range(len(t)):
+            if t[j] not in dict_1:
+                return False
+            else:
+                dict_1[t[j]] -= 1
+
+        return all(x== 0 for x in dict_1.values())
+ 
+        
+#         for k in range(len(t)):
+#             if t[k] not in dict_2:
+#                 dict_2[t[k]] = 1
+#             else:
+#                 dict_2[t[k]] +=1
+                
+#         return dict_1 == dict_2
+                
+        
+            
+            
+            
+            
+        
+        
+        
+        
+        
+#         if len(s) != len(t):
+#             return False
+        
+#         return sorted(s) == sorted(t)
+    
+    #solution using hash map
+        
+    
+    
+    
  
             
          
