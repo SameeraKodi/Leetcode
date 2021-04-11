eef3cfbb7240a309098a22326e67e1ffca0f58cf
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-11 17:37:49 -0400

    Time: 64 ms (39.79%), Space: 15 MB (87.35%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,18 +1,42 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-       #since multiple transactions are allowed we have to keep track of all possible profits hence the final list
+        
         profit = 0
-        final = []
+        result = []
         
         for i in range(1,len(prices)):
             buy_price = prices[i-1]
-            if prices[i] > buy_price: #this is condition implying selling
-                profit = max(profit,prices[i]- buy_price)
-                final.append(profit)
+            if prices[i] > buy_price:
+                profit = max(profit,prices[i]-buy_price)
+                result.append(profit)
                 profit = 0
-
-
-            else: #this is condition implying no sale so change in buy price
+            else:
                 buy_price = prices[i]
+        
+        return sum(result)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#        #since multiple transactions are allowed we have to keep track of all possible profits hence the final list
+#         profit = 0
+#         final = []
+        
+#         for i in range(1,len(prices)):
+#             buy_price = prices[i-1]
+#             if prices[i] > buy_price: #this is condition implying selling
+#                 profit = max(profit,prices[i]- buy_price)
+#                 final.append(profit)
+#                 profit = 0
+
+
+#             else: #this is condition implying no sale so change in buy price
+#                 buy_price = prices[i]
                 
-        return sum(final)
+#         return sum(final)
