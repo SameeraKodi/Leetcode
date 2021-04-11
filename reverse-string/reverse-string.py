8ae1619b2d1135f0c3700d5e3a067d935bf195d2
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-11 10:15:46 -0400

    Time: 192 ms (75.34%), Space: 18.7 MB (47.24%) - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -1,18 +1,38 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
+        
+        #2 pointer method
+        begin = 0
+        end = len(s)-1
+        
+        while begin <= end:
+            temp = s[begin]
+            s[begin] = s[end]
+            s[end] = temp
+            begin += 1
+            end -= 1
+        
+        
+        
+        
+        
+        
+        
         """
         Do not return anything, modify s in-place instead.
         """
-        l = len(s)
-        mid_len = int(len(s)/2)
+#         l = len(s)
+#         mid_len = int(len(s)/2)
         
-#         if mid_len%2 == 0:
-        for i in range(mid_len):
+# #         if mid_len%2 == 0:
+#         for i in range(mid_len):
             
-            temp = s[i]
-            s[i] = s[l-(i+1)]
-            s[l-(i+1)] = temp
+#             temp = s[i]
+#             s[i] = s[l-(i+1)]
+#             s[l-(i+1)] = temp
             
-        return s
+#         return s
+
+       
             
         
