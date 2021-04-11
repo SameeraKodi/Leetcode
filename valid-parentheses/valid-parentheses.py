289172a3f8bb96ecc6c509198e48545ed24982ff
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-11 10:08:11 -0400

    Time: 32 ms (58.20%), Space: 14.3 MB (65.21%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -3,22 +3,78 @@ class Solution:
         stack = []
         
         for char in s:
-            if char =="(" or char =="[" or char =="{":
+            if char == '(' or char == '[' or char == '{':
                 stack.append(char)
-            elif stack: 
-                prev_char = stack.pop()
-                if prev_char == "(" and char != ")":
+            
+            elif len(stack) != 0:
+                prev = stack.pop()
+                if char == ")" and prev != "(":
+                    return False
+                elif char == "]" and prev != "[":
+                    return False
+                elif char == "}" and prev != "{":
                     return False
                 
-                    
-                elif prev_char == "{" and char != "}":
-            
-                    return False
-                elif prev_char == "[" and char != "]":
-                    return False
-
             else:
                 return False
-                
         return len(stack) == 0
+                
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         stack = []
+        
+#         for char in s:
+#             if char =="(" or char =="[" or char =="{":
+#                 stack.append(char)
+#             elif stack: 
+#                 prev_char = stack.pop()
+#                 if prev_char == "(" and char != ")":
+#                     return False
+                
+                    
+#                 elif prev_char == "{" and char != "}":
+            
+#                     return False
+#                 elif prev_char == "[" and char != "]":
+#                     return False
+
+#             else:
+#                 return False
+                
+#         return len(stack) == 0
+
+            
         
