93e05a3915cf335a7ee73b0661b881808d136bd1
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-12 10:36:04 -0400

    Time: 164 ms (66.09%), Space: 20.6 MB (66.69%) - LeetHub

--- search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -3,18 +3,67 @@ class Solution:
         if len(matrix) == 0 or len(matrix[0]) == 0:
             return False
         
-        row = 0
-        col = len(matrix[0])-1
+        row_ptr = 0
+        col_ptr = len(matrix[0])-1
         
-        while row < len(matrix) and col >= 0:
-            if matrix[row][col] == target:
-                return True
-            elif matrix[row][col] > target:
-                col -= 1
+        while row_ptr < len(matrix) and col_ptr >= 0:
+            if target < matrix[row_ptr][col_ptr]:
+                col_ptr -= 1
+            elif target > matrix[row_ptr][col_ptr]:
+                row_ptr += 1
             else:
-                row += 1
-                
+                return True
+            
         return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if len(matrix) == 0 or len(matrix[0]) == 0:
+#             return False
+        
+#         row = 0
+#         col = len(matrix[0]) - 1
+        
+#         while row < len(matrix) and col >= 0:
+#             if matrix[row][col] == target:
+#                 return True
+#             elif matrix[row][col] > target:
+#                 col -= 1
+#             else:
+#                 row += 1
+                
+#         return False
         
         
         
