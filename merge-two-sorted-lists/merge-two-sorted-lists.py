e0174c5bbe83254d93e0adeeea8156ce4274d507
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-11 20:07:20 -0400

    Time: 40 ms (42.18%), Space: 14.4 MB (32.24%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -8,7 +8,7 @@ class Solution:
         head = ListNode(0)
         current = head
         
-        while l1!= None and l2 !=None:
+        while l1 != None and l2!= None:
             if l1.val < l2.val:
                 current.next = l1
                 l1 = l1.next
@@ -17,17 +17,70 @@ class Solution:
                 current.next = l2
                 l2 = l2.next
             
-            current = current.next 
+            current = current.next
             
         if l1 != None:
             current.next = l1
             l1 = l1.next
-        
         if l2 != None:
             current.next = l2
             l2 = l2.next
             
         return head.next
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         head = ListNode(0)
+#         current = head
+        
+#         while l1!= None and l2 !=None:
+#             if l1.val < l2.val:
+#                 current.next = l1
+#                 l1 = l1.next
+                
+#             else:
+#                 current.next = l2
+#                 l2 = l2.next
+            
+#             current = current.next 
+            
+#         if l1 != None:
+#             current.next = l1
+#             l1 = l1.next
+        
+#         if l2 != None:
+#             current.next = l2
+#             l2 = l2.next
+            
+#         return head.next
             
 
 
