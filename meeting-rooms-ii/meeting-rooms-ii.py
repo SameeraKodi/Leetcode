dd9a426645a2bd3cf94d9229aa71c308d80372b1
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-11 21:53:08 -0400

    Time: 72 ms (87.34%), Space: 17.5 MB (22.72%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.py
+++ meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,24 +1,63 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        count = 1
         if len(intervals) == 0:
             return 0
         
         start,end = list(list(zip(*intervals))[0]),list(list(zip(*intervals))[1])
-
         start.sort()
         end.sort()
-        count = 1
 
+        
         end_ptr = 0
         
         for st_ptr in range(1,len(start)):
             if start[st_ptr] >= end[end_ptr]:
-                
                 end_ptr += 1
             else:
-                count += 1
+                count +=1
                 
         return count
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if len(intervals) == 0:
+#             return 0
+        
+#         start,end = list(list(zip(*intervals))[0]),list(list(zip(*intervals))[1])
+
+#         start.sort()
+#         end.sort()
+#         count = 1
+
+#         end_ptr = 0
+        
+#         for st_ptr in range(1,len(start)):
+#             if start[st_ptr] >= end[end_ptr]:
+                
+#                 end_ptr += 1
+#             else:
+#                 count += 1
+                
+#         return count
     
             
             
