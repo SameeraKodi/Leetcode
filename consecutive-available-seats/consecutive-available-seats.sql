e21f01fe663295e943c31d45823c40451a95c42a
SameeraKodi <36885980+SameeraKodi@users.noreply.github.com>
2021-04-23 22:44:45 -0400

    Time: 662 ms (57.50%), Space: 0B (100.00%) - LeetHub

---
+++ consecutive-available-seats/consecutive-available-seats.sql
@@ -0,0 +1,8 @@
+select seat_id
+from cinema
+where free = 1 and (seat_id + 1 in (select seat_id
+from cinema
+where free = 1 ) or seat_id - 1 in (select seat_id
+from cinema
+where free = 1 ))
+order by seat_id asc
